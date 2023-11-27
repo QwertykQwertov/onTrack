fce9aa26a86c831e49e68d57b075eefe5f03e8fd
Qwertyk Qwertov <qwertykqwertov@mail.ru>
2023-11-27 22:09:30 +0300

    feat: refactor activity form

--- src/assets/pages/TheActivities.vue
+++ src/assets/pages/TheActivities.vue
@@ -1,8 +1,7 @@
 <script setup>
 import ActivityItem from '@/components/ActivityItem.vue'
-import BaseButton from '@/components/BaseButton.vue'
+import TheActivityForm from '@/components/TheActivityForm.vue'
 import { validateActivities, isActivityValid } from '@/validators'
-import { PlusIcon } from '@heroicons/vue/24/outline'
 
 defineProps({
   activities: {
@@ -15,8 +14,6 @@ const emit = defineEmits({
   createActivity: isActivityValid,
   deleteActivity: isActivityValid
 })
-
-let newActivity = ''
 </script>
 <template>
   <div>
@@ -28,17 +25,6 @@ let newActivity = ''
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <form @submit.prevent="emit('createActivity', newActivity)" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
-      <input
-        type="text"
-        :value="newActivity"
-        class="w-full rounded border px-4 text-xl"
-        placeholder="Activity name"
-        @input="newActivity = $event.target.value"
-      />
-      <BaseButton>
-        <PlusIcon class="h-8" />
-      </BaseButton>
-    </form>
+    <TheActivityForm @submit="emit('createActivity', $event)"/>
   </div>
 </template>
